11c4f5d7d9549d65cef00f9081d20e47780090fe
hlnicks <hlnicks@icloud.com>
2022-07-08 22:10:02 -0400

    created arrays and new input function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,59 @@
-// when user clicks on button, prompts are presented for password criteria:
-// prompt 1: how long does user want password to be. at least 8, not more than 128
-// prompt 2: asks user if they want lowercase letters to be included
-// prompt 3: asks user if they want uppercase letters to be included
-// prompt 4: asks user if they want numbers to be included
-// prompt 5: asks user if they want special characters to be included
-// at least one character from each desired character type should be included in generated password
-// once generated, password should be displayed in an alert or on page
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersArr= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArr = ["~", "`", "!", "@", "#", "$","%","^", "&", "*", "(", ")", "-", "_", "+", "=", ";", ":", "'", '"', "<", ",", ".", ">", "?", "/", "[", "{", "]", "}"];
 
 
-function promptUser() {
+
+function createPassword() {
 
   // password length
   var promptLength = window.prompt("Please enter desired password length. Choose a number between 7 - 129.");
   promptLength = parseInt(promptLength);
-  if (promptLength < 8 || promptLength > 128){
+  if (promptLength < 8 || promptLength > 128 || isNaN(promptLength)){
     window.alert("Invalid option. Please enter a number between 7 and 129.");
-    return promptUser();
+    return createPassword();
   }
-  if (isNaN(promptLength)) {
-    window.alert("Please select a numerical value.")
-    return promptUser();
-  }
-
-  // lowercase letters
-  var promptLower = window.confirm("Do you want your password to contain lowercase letters?");
-
-  // uppercase letters
-  var promptUpper = confirm("Do you want your password to contain uppercase letters?");
-
-  // numbers
-  var promptNumber = confirm("Do you want your password to contain numbers?");
-
-  // special characters
-  var promptSpecial = confirm("Do you want your password to contain special characters?");
+  return promptLength;
 };
 
 
+// create separate functions??
+
+function userCriteria() {
+  var promptLength = createPassword();
+  var inputArr = [];
+
+  // lowercase letters
+  var promptLower = confirm("Do you want your password to contain lowercase letters?");
+  if (promptLower) {
+     // if true, include lowercase letters
+  };
+
+
+  // uppercase letters
+  var promptUpper = window.confirm("Do you want your password to contain uppercase letters?");
+  if (promptUpper) {
+    // if true, include uppercase letters
+  };
+
+
+  // numbers
+  var promptNumber = window.confirm("Do you want your password to contain numbers?");
+  if (promptNumber) {
+    // if true, include numbers
+  };
+
+
+  // special characters
+  var promptSpecial = window.confirm("Do you want your password to contain special characters?");
+  if (promptSpecial) {
+    // if true, include symbols
+  };
+};
+
+
+// v  FROM INITIAL FILES  v //
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
